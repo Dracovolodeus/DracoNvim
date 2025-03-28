2cb25a393ec3c6c7ac6a76a803c56952c6d83862
Dracovolodeus <bananaed12@gmail.com>
2025-03-28 22:49:52 +0700

    enable treesitter indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,16 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   config = function()
-    require'nvim-treesitter.configs'.setup {
+    require'nvim-treesitter.configs'.setup{
       ensure_installed = {
         "python", "rust", "c", "bash", "lua", "json", "css", "sql", "toml"
       },
       sync_install = false,
       auto_install = true,
       highlight = {
+        enable = true,
+      },
+      indent = {
         enable = true,
       },
     }
